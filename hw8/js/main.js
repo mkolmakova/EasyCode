5c96ff43da71f0f394399637d657ec8ab100dd3a
MKolmakova <imariwka@gmail.com>
2019-01-16 18:18:56 +0200

    add hw8

---
+++ hw8/js/main.js
@@ -0,0 +1,14 @@
+$(document).on('click', '.tabs_nav-link', function(e){
+  e.preventDefault();
+  var tabId = $(this).attr('href');
+  $(this).addClass('active');
+  $(this).parent('li').siblings().find('.tabs_nav-link').removeClass('active');
+  $(tabId).show(400);
+  $(tabId).siblings('.tab_content').hide(400);
+});
+
+$('.testimonials_carousel').slick({
+	prevArrow: '<button type="button" class="slick-prev"><span><</span></button>',
+	nextArrow: '<button type="button" class="slick-next"><span>></span></button>',
+	dots:true
+});
